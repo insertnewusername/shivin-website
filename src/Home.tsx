ce23ade3084d455946c662f0f445ebd88c6d98ec
penrose-programmer <syedtalhahahmed@gmail.com>
2026-09-06 09:21:46 +1000

    email field

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'wouter'
+import { useLocation } from 'wouter'
 import './App.css'
 
 
@@ -11,9 +11,12 @@ function Home() {
             <h1 className='hero-heading'>Now this is <span className='hero-marker-text'>Rocket Science</span></h1>
             <p className='hero-subheading'>A crafty rocketry competition for <br/> daring Australian high school students</p>
 
-            <Link href="/register">
-                <button className='hero-button'>Register Now</button>
-            </Link>
+            <form>
+              <div className='hero-form-container'>
+                <input type="email" placeholder="griffin@example.com" className='text-input'></input>
+                <input type="submit" value="Register" className='hero-button'></input>
+              </div>
+            </form>
         </header>
     </>
   )

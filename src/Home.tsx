fdaadaccf8eed8b3dbf1d64c0112216b451d0bf9
penrose-programmer <syedtalhahahmed@gmail.com>
2026-09-06 12:40:11 +1000

    major updates to the hero-page, including a new animated 3D rocket wireframe

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,3 @@
-import { useLocation } from 'wouter'
 import './App.css'
 
 
@@ -6,17 +5,26 @@ function Home() {
   return (
     <>
         <header className='hero-container'>
-            <p>Applications Close Soon!</p>
+            <div className="hero-text-container">
+                <p className='hero-badge'>Applications Close Soon!</p>
 
-            <h1 className='hero-heading'>Now this is <span className='hero-marker-text'>Rocket Science</span></h1>
-            <p className='hero-subheading'>A crafty rocketry competition for <br/> daring Australian high school students</p>
+              <h1 className='hero-heading'>Now this is <span className='hero-marker-text'>Rocket Science</span></h1>
+              <p className='hero-subheading'>A crafty rocketry competition for <br/> daring Australian high school students</p>
 
-            <form>
-              <div className='hero-form-container'>
-                <input type="email" placeholder="griffin@example.com" className='text-input'></input>
-                <input type="submit" value="Register" className='hero-button'></input>
-              </div>
-            </form>
+              <form>
+                <div className='hero-form-container'>
+                  <input type="email" placeholder="griffin@example.com" className='text-input'></input>
+                  <input type="submit" value="Register" className='hero-button'></input>
+                </div>
+              </form>
+            </div>
+            <div className='hero-rocket-container'>
+              <video autoPlay loop width="607" height="1080">
+                <source src="/public/rocket-anim.webm" type="video/webm" />
+                Your browser does not support this
+              </video>
+            </div>
+            
         </header>
     </>
   )
